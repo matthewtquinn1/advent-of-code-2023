ab1ab3009d9f170c2d608caf7c0c352c47ac4468
Matthew Quinn <matthewtquinn1@gmail.com>
2023-12-28 19:39:21 +0000

    Complete challenge 1.

--- pkg/challenges/1.go
+++ pkg/challenges/1.go
@@ -3,13 +3,32 @@ package challenges
 import (
 	"aoc-2023/pkg/inpututils"
 	"fmt"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
 func GetAnswerOne() {
 	inputLines := inpututils.GetFileInput("1-input.txt")
 
+	// Regex for finding single-digit numbers in a string.
+	re := regexp.MustCompile("[0-9]")
+
+	totalValue := 0
 	for _, line := range strings.Fields(inputLines) {
-		fmt.Println(line)
+
+		// Extract all numbers from line into an array of strings.
+		numbers := re.FindAllString(line, -1)
+
+		// Get first and last number as strings, concat them and convert that into an int.
+		lineValue, convErr := strconv.Atoi(numbers[0] + numbers[len(numbers)-1])
+		if convErr != nil {
+			fmt.Println(convErr)
+		}
+
+		// Sum each line value.
+		totalValue += lineValue
 	}
+
+	fmt.Println("Challenge 1 - total value is ", totalValue)
 }
